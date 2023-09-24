c31bed651381c5b85be7faf84f6759de23c115ea
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2023-09-24 00:45:31 +0000

    cleanup with a little but more docs

--- readme.md
+++ readme.md
@@ -1,20 +1,30 @@
 # Introduction
 
-Figures out the type of some json object.
+This is a tool that ought to make encoding and decoding data in ts less painful (not to say it is because people don't really do that).
+The tool would be in 3 parts:
 
-I initially started out by just checking the types of the json data, but quickly realized that this wasn't a good solution.
+1. TS types generator from json.
+2. Codec generators from TS types
+3. Codec generators from json (JSON -> TS types -> Codecs)
 
-What I'm thinking of doing is to eagerly traverse the json and produce wide types that can be narrowed down at the end.
+Should come with some additional utilities that enable you generate (part 1 or 3) directly from an http call that produces json.
+
+## WIP
+
+This was built overnight because I'm lazy. I want to be able to auto describe the response of some HTTP REST endpoint.
+I initially started out by just traversing the json data and using `typeof x` to build an AST for the data in the JSON.
+I've quickly realized that this isn't a good solution.
+
+What I'm thinking of doing next is to traverse the json and eagerly produce wide types that can be narrowed down at the end.
+In my mind, this is a much better solution because it allows the comparison to be done with a more controlled and consistent structure
+which makes it easier.
+
 Let's understand this with an example.
 
 ```json
-const json = {
-    signals: [
-        { action: 1 },
-        { action: 0 },
-        { action: 1 }
-    ],
-    sender: 'mxvd'
+{
+    "signals": [{ "action": 1 }, { "action": 0 }, { "action": 1 }],
+    "sender": "mxvd"
 }
 ```
 
@@ -32,4 +42,7 @@ interface MyType {
 
 This is apparent to us because we can see the consistency at a glance, but imagine a case where the object was much larger.
 We would still be able to figure out that they are all the same but it would require a little bit more time because we would need to examine quite a lot of data.
-This is true as well for this tooling.
+
+The problem with JSON objects is that they can represent one of two types: a `Dictionary<K, V>` or a `Record`. As you can imagine, there's no straighforward way to
+know if an object is a Record or a Dictionary early on. There are a couple of ways we could guess this information like checking if all the values in the object are of the same type.
+However, assuming the wider type, in this case - Record, is always the safest choice.
--- src/lib/grokker.ts
+++ src/lib/grokker.ts
@@ -1,184 +1,184 @@
-import ts from 'typescript'
+import * as ts from 'typescript'
 
 abstract class Type {
-	abstract toString(): string
+    abstract toString(): string
 }
 type Field = [string, Type]
 class NumberType extends Type {
-	toString(): string {
-		return 'NumberType'
-	}
+    toString(): string {
+        return 'NumberType'
+    }
 }
 const numberType: NumberType = new NumberType()
 class BigIntType extends Type {
-	toString(): string {
-		return 'BigIntType'
-	}
+    toString(): string {
+        return 'BigIntType'
+    }
 }
 const bigIntType: BigIntType = new BigIntType()
 class StringType extends Type {
-	toString(): string {
-		return 'StringType'
-	}
+    toString(): string {
+        return 'StringType'
+    }
 }
 const stringType: StringType = new StringType()
 class BoolType extends Type {
-	toString(): string {
-		return 'BoolType'
-	}
+    toString(): string {
+        return 'BoolType'
+    }
 }
 const boolType: BoolType = new BoolType()
 class UnknownType extends Type {
-	toString(): string {
-		return 'UnknownType'
-	}
+    toString(): string {
+        return 'UnknownType'
+    }
 }
 const unknownType: UnknownType = new UnknownType()
 class NeverType extends Type {
-	toString(): string {
-		return 'NeverType'
-	}
+    toString(): string {
+        return 'NeverType'
+    }
 }
 const neverType: NeverType = new NeverType()
 class NullableType extends Type {
-	constructor(public tpe: Type) {
-		super()
-	}
-	toString(): string {
-		return `NullableType<${this.tpe.toString()}>`
-	}
+    constructor(public tpe: Type) {
+        super()
+    }
+    toString(): string {
+        return `NullableType<${this.tpe.toString()}>`
+    }
 }
 const nullableType = (tpe: Type) => new NullableType(tpe)
 class ArrayType extends Type {
-	constructor(public tpe: Type) {
-		super()
-	}
-	toString(): string {
-		return `ArrayType<${this.tpe.toString()}>`
-	}
+    constructor(public tpe: Type) {
+        super()
+    }
+    toString(): string {
+        return `ArrayType<${this.tpe.toString()}>`
+    }
 }
 const arrayType = (tpe: Type) => new ArrayType(tpe)
 class TupleNType extends Type {
-	constructor(public tpes: Array<Type>) {
-		super()
-	}
-	toString(): string {
-		return `Tuple<${this.tpes.map(each => each.toString()).join(', ')}>`
-	}
+    constructor(public tpes: Array<Type>) {
+        super()
+    }
+    toString(): string {
+        return `Tuple<${this.tpes.map(each => each.toString()).join(', ')}>`
+    }
 }
 const tupleNTypeType = (tpes: Array<Type>) => new TupleNType(tpes)
 class DictType extends Type {
-	constructor(public valueType: Type) {
-		super()
-	}
-	toString(): string {
-		return `DictType<${this.valueType.toString()}>`
-	}
+    constructor(public valueType: Type) {
+        super()
+    }
+    toString(): string {
+        return `DictType<${this.valueType.toString()}>`
+    }
 }
 const dictType = (valTpe: Type) => new DictType(valTpe)
 class RecordType extends Type {
-	constructor(public fields: Array<Field>) {
-		super()
-	}
-	toString(): string {
-		return `RecordType(${this.fields.map(field => `${field[0]} -> ${field[1].toString()}`).join(', ')})`
-	}
+    constructor(public fields: Array<Field>) {
+        super()
+    }
+    toString(): string {
+        return `RecordType(${this.fields.map(field => `${field[0]} -> ${field[1].toString()}`).join(', ')})`
+    }
 }
 const recordType = (fields: Array<Field>) => new RecordType(fields)
 
 function to(node: Type): string {
-	return ''
+    return ''
 }
 
 /** Takes a json content and understands the fields and its types.
  * The reason we recieve a string is to prevent trying to grok circular or non-serializable objects.
  * */
-export default function grok(json: string): string {
-	const struct = parseAndExtract(JSON.parse(json))
-	console.log(struct.toString())
-	return json
+export function grok(json: string): string {
+    const struct = parseAndExtract(JSON.parse(json))
+    console.log(struct.toString())
+    return json
 }
 
 export function parseAndExtract(json: any): Type {
-	if (json === null) return nullableType(unknownType)
+    if (json === null) return nullableType(unknownType)
 
-	switch (typeof json) {
-		case 'object':
-			if (Array.isArray(json)) {
-				if (!hasValues(json)) {
-					// can't tell the type of the elements
-					return arrayType(unknownType)
-				}
-				if (hasUniformValues(json)) {
-					// if all elements are the same, use the type of the first element
-					const innerType = parseAndExtract(json[0])
-					return arrayType(innerType)
-				}
+    switch (typeof json) {
+        case 'object':
+            if (Array.isArray(json)) {
+                if (!hasValues(json)) {
+                    // can't tell the type of the elements
+                    return arrayType(unknownType)
+                }
+                if (hasUniformValues(json)) {
+                    // if all elements are the same, use the type of the first element
+                    const innerType = parseAndExtract(json[0])
+                    return arrayType(innerType)
+                }
 
-				// This is the case of a TupleN
-				return tupleNTypeType(json.map(parseAndExtract))
-			} else {
-				// empty object is treated like an empty array because it's elements are unknown
-				if (!hasValues(json)) {
-					dictType(unknownType)
-				} else if (hasUniformValues(json)) {
-					for (let k in json) {
-						if (json.hasOwnProperty(k)) {
-							return dictType(typeof k)
-						}
-					}
-				}
-				const fields = toFields(json)
-				return recordType(fields)
-			}
-		case 'string':
-			return stringType
-		case 'boolean':
-			return boolType
-		case 'number':
-			return numberType
-		case 'bigint':
-			return bigIntType
+                // This is the case of a TupleN
+                return tupleNTypeType(json.map(parseAndExtract))
+            } else {
+                // empty object is treated like an empty array because it's elements are unknown
+                if (!hasValues(json)) {
+                    dictType(unknownType)
+                } else if (hasUniformValues(json)) {
+                    for (let k in json) {
+                        if (json.hasOwnProperty(k)) {
+                            return dictType(typeof k)
+                        }
+                    }
+                }
+                const fields = toFields(json)
+                return recordType(fields)
+            }
+        case 'string':
+            return stringType
+        case 'boolean':
+            return boolType
+        case 'number':
+            return numberType
+        case 'bigint':
+            return bigIntType
 
-		// unsupported data types
-		case 'symbol':
-			throw Error(`Unsupported Type!!! ${typeof json}`)
-		case 'function':
-			throw Error(`Unsupported Type!!! ${typeof json}`)
-		case 'undefined':
-			throw Error(`Unsupported Type!!! ${typeof json}`)
-		default:
-			throw Error(`Unsupported Type!!! ${typeof json}`)
-	}
+        // unsupported data types
+        case 'symbol':
+            throw Error(`Unsupported Type!!! ${typeof json}`)
+        case 'function':
+            throw Error(`Unsupported Type!!! ${typeof json}`)
+        case 'undefined':
+            throw Error(`Unsupported Type!!! ${typeof json}`)
+        default:
+            throw Error(`Unsupported Type!!! ${typeof json}`)
+    }
 }
 
 // checks if the values are of a uniform type
-// in the case of where the values are objects, we want to compare an ordered set of the keys 
+// in the case of where the values are objects, we want to compare an ordered set of the keys
 function hasUniformValues<T>(input: Array<T> | object): boolean {
-	if (Array.isArray(input)) {
-		if (!hasValues(input)) return true // a vacuous truth
-		const first = typeof input[0]
-		return input.every(el => typeof el === first)
-	} else {
-		let firstTpe: string = ''
-		for (let key in input) {
-			if (input.hasOwnProperty(key)) {
-				if (firstTpe !== '' && typeof (input as any)[key] !== firstTpe) {
-					return false
-				} else firstTpe = typeof (input as any)[key]
-			}
-		}
-		return true
-	}
+    if (Array.isArray(input)) {
+        if (!hasValues(input)) return true // a vacuous truth
+        const first = typeof input[0]
+        return input.every(el => typeof el === first)
+    } else {
+        let firstTpe: string = ''
+        for (let key in input) {
+            if (input.hasOwnProperty(key)) {
+                if (firstTpe !== '' && typeof (input as any)[key] !== firstTpe) {
+                    return false
+                } else firstTpe = typeof (input as any)[key]
+            }
+        }
+        return true
+    }
 }
 
 function hasValues<T>(input: Array<T> | object): boolean {
-	if (Array.isArray(input)) return input.length > 0
-	else
-		for (let key in input) {
-			if (input.hasOwnProperty(key)) return true
-		}
-	return false
+    if (Array.isArray(input)) return input.length > 0
+    else
+        for (let key in input) {
+            if (input.hasOwnProperty(key)) return true
+        }
+    return false
 }
 
 /**
@@ -188,76 +188,9 @@ function hasValues<T>(input: Array<T> | object): boolean {
  * @param json object
  */
 function toFields(json: object): Field[] {
-	const _j: any = json //
-	const fields: Field[] = Object.keys(_j)
-		.sort((a, b) => (a > b ? 1 : b > a ? -1 : 0))
-		.map(field => [field, parseAndExtract(_j[field])])
-	return fields
+    const _j: any = json //
+    const fields: Field[] = Object.keys(_j)
+        .sort((a, b) => (a > b ? 1 : b > a ? -1 : 0))
+        .map(field => [field, parseAndExtract(_j[field])])
+    return fields
 }
-
-const json = {
-	products: [
-		{
-			id: 7,
-			title: 'Samsung Galaxy Book',
-			description: 'Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched',
-			price: 1499,
-			discountPercentage: 4.15,
-			rating: 4.25,
-			stock: 50,
-			brand: 'Samsung',
-			category: 'laptops',
-			thumbnail: 'https://i.dummyjson.com/data/products/7/thumbnail.jpg',
-			images: [
-				'https://i.dummyjson.com/data/products/7/1.jpg',
-				'https://i.dummyjson.com/data/products/7/2.jpg',
-				'https://i.dummyjson.com/data/products/7/3.jpg',
-				'https://i.dummyjson.com/data/products/7/thumbnail.jpg'
-			]
-		},
-		{
-			id: 8,
-			title: 'Microsoft Surface Laptop 4',
-			description:
-				'Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.',
-			price: 1499,
-			discountPercentage: 10.23,
-			rating: 4.43,
-			stock: 68,
-			brand: 'Microsoft Surface',
-			category: 'laptops',
-			thumbnail: 'https://i.dummyjson.com/data/products/8/thumbnail.jpg',
-			images: [
-				'https://i.dummyjson.com/data/products/8/1.jpg',
-				'https://i.dummyjson.com/data/products/8/2.jpg',
-				'https://i.dummyjson.com/data/products/8/3.jpg',
-				'https://i.dummyjson.com/data/products/8/4.jpg',
-				'https://i.dummyjson.com/data/products/8/thumbnail.jpg'
-			]
-		},
-		{
-			id: 10,
-			title: 'HP Pavilion 15-DK1056WM',
-			description:
-				'HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10',
-			price: 1099,
-			discountPercentage: 6.18,
-			rating: 4.43,
-			stock: 89,
-			brand: 'HP Pavilion',
-			category: 'laptops',
-			thumbnail: 'https://i.dummyjson.com/data/products/10/thumbnail.jpeg',
-			images: [
-				'https://i.dummyjson.com/data/products/10/1.jpg',
-				'https://i.dummyjson.com/data/products/10/2.jpg',
-				'https://i.dummyjson.com/data/products/10/3.jpg',
-				'https://i.dummyjson.com/data/products/10/thumbnail.jpeg'
-			]
-		}
-	],
-	total: 3,
-	skip: 0,
-	limit: 3
-}
-
-grok(JSON.stringify(json))
